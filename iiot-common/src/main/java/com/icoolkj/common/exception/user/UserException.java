55af60d26a6e7a2842a8512903faca5285163f62
icoolkj <icoolkj@163.com>
2023-02-27 22:01:50 +0800

    工业物联网平台

---
+++ iiot-common/src/main/java/com/icoolkj/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.icoolkj.common.exception.user;
+
+import com.icoolkj.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
