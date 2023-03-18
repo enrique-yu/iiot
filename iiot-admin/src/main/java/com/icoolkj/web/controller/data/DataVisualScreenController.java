badbc86e7b63b83cfb783a654d33935d5fc9be26
x_daipeng <dp214084201>
2023-03-18 10:26:29 +0800

    数据可视化中心

---
+++ iiot-admin/src/main/java/com/icoolkj/web/controller/data/DataVisualScreenController.java
@@ -0,0 +1,20 @@
+package com.icoolkj.web.controller.data;
+
+import com.icoolkj.common.core.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 数据视觉中心Controller
+ *
+ * @author icoolkj
+ * @date 2023-03-17
+ */
+@RestController
+@RequestMapping("/dataVisual")
+public class DataVisualScreenController extends BaseController
+{
+
+
+
+}
